36b10bdb5b070f55a588a749f578ba0dbc782884
Ashish Prabhune <ashuprabhune@gmail.com>
2019-12-18 19:13:14 -0500

    Initial Project Setup

---
+++ src/main/java/guru/springframework/springbootpetclinic/SpringbootPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springbootpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/springbootpetclinic/SpringbootPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springbootpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
