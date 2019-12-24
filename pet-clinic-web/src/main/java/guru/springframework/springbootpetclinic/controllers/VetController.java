774fb9f5a73cd855e555fac25f5a685f68ca8cfd
Ashish Prabhune <ashuprabhune@gmail.com>
2019-12-24 00:33:38 -0500

    Added functionality to list Vets. Closes #15

--- pet-clinic-web/src/main/java/guru/springframework/springbootpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/springbootpetclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package guru.springframework.springbootpetclinic.controllers;
 
+import guru.springframework.springbootpetclinic.services.map.VetMapService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetMapService vetMapService;
+
+    @Autowired
+    public VetController(VetMapService vetMapService) {
+        this.vetMapService = vetMapService;
+    }
 
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
-
+    public String listVets(Model model){
+        model.addAttribute("vets",vetMapService.findAll());
         return "vets/index";
     }
 }
