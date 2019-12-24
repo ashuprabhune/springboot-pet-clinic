2f8ecdf0162e08521f0b533c39e639f35d7ec0c1
Ashish Prabhune <ashuprabhune@gmail.com>
2019-12-24 00:24:26 -0500

    Added functionality to list owners. Closes #16

--- pet-clinic-web/src/main/java/guru/springframework/springbootpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/springbootpetclinic/controllers/OwnerController.java
@@ -1,14 +1,26 @@
 package guru.springframework.springbootpetclinic.controllers;
 
+import guru.springframework.springbootpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired //No need to do that
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({""," /","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
+
         return "owners/index";
     }
 }
