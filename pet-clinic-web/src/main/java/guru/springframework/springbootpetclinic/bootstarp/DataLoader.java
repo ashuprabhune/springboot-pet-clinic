6f6ddd573e5e18778f22b07822ff69015769caee
Ashish Prabhune <ashuprabhune@gmail.com>
2020-01-21 23:35:53 -0500

    Added PetTypes in DataLoader. closes #32

--- pet-clinic-web/src/main/java/guru/springframework/springbootpetclinic/bootstarp/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/springbootpetclinic/bootstarp/DataLoader.java
@@ -1,28 +1,42 @@
 package guru.springframework.springbootpetclinic.bootstarp;
 
 import guru.springframework.springbootpetclinic.model.Owner;
+import guru.springframework.springbootpetclinic.model.PetType;
 import guru.springframework.springbootpetclinic.model.Vet;
 import guru.springframework.springbootpetclinic.services.OwnerService;
+import guru.springframework.springbootpetclinic.services.PetTypeService;
 import guru.springframework.springbootpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DataLoader implements CommandLineRunner {
+public class  DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired //No need to do that
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogPetType  = petTypeService.save(dog);
+
+
+        PetType cat = new PetType();
+        cat.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner = new Owner();
         owner.setFirstName("Ashish");
         owner.setLastName("Prabhune");
