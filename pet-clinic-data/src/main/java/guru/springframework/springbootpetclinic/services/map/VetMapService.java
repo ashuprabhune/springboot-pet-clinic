ead2849d3244789bc933dd02703463bc9bd2d21d
Ashish Prabhune <ashuprabhune@gmail.com>
2020-01-23 23:53:37 -0500

    Editied VetServiceMap to handle bad specialty object.

--- pet-clinic-data/src/main/java/guru/springframework/springbootpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/springbootpetclinic/services/map/VetMapService.java
@@ -1,6 +1,8 @@
 package guru.springframework.springbootpetclinic.services.map;
 
+import guru.springframework.springbootpetclinic.model.Specialty;
 import guru.springframework.springbootpetclinic.model.Vet;
+import guru.springframework.springbootpetclinic.services.SpecialtiesService;
 import guru.springframework.springbootpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,13 @@ import java.util.Set;
 
 @Service
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtiesService specialtiesService;
+
+    public VetMapService(SpecialtiesService specialtiesService) {
+        this.specialtiesService = specialtiesService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -30,6 +39,16 @@ public class VetMapService extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if(object.getSpecialties().size()>0){
+            object.getSpecialties().forEach(specialty -> {
+                if(specialty.getId() == null){
+                    Specialty saveSpecialty = specialtiesService.save(specialty);
+                    saveSpecialty.setId(specialty.getId());
+                }
+            });
+        }
+
+
         return super.save(object);
     }
 }
