40b247178d7fabda13f8d365931e6e6e7d2f666a
Ashish Prabhune <ashuprabhune@gmail.com>
2020-04-29 00:33:01 -0400

    Updated Visit entity with JPA. closes #38

--- pet-clinic-data/src/main/java/guru/springframework/springbootpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/springbootpetclinic/model/Pet.java
@@ -2,6 +2,9 @@ package guru.springframework.springbootpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -20,6 +23,19 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
